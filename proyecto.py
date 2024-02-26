e578c546c33c277fd8ad1ba03209428365658a70
jose020712 <jluismatos68@gmail.com>
2024-02-25 19:23:33 -0500

    first commit

---
+++ proyecto.py
@@ -0,0 +1,12 @@
+# Clase para TODO tipo de animal
+
+class Animal:
+    def __init__(self,cantidad,periodo_gestacion, nombre_cientifico, nombre_comun,jaula,region_procedencia):
+        self.cantidad           = cantidad
+        self.periodo_gestacion  = periodo_gestacion
+        self.nombre_cientifico  = nombre_cientifico
+        self.nombre_comun       = nombre_comun
+        self.jaula              = jaula
+        self.region_procedencia = region_procedencia
+
+
